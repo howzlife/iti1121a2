116e7410078ff93940effc954a69a8025ec36f6d
Uiri <uiri00@gmail.com>
2014-02-06 21:13:57 -0500

    Add shuffle implementation to RandomPermutation

--- RandomPermutation.java
+++ RandomPermutation.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 class RandomPermutation {
     private int[] arr;
 
@@ -18,7 +20,17 @@ class RandomPermutation {
     /* Public Methods */
 
     public void shuffle() {
-        
+        Random generator = new Random();
+        // Starts at the end of the array (minus 1 so the space is always at the
+        // end), randomly selecting an earlier index in the array and switching
+        // the values at the current point and the randomly selected point
+        for (int k = arr.length - 2;k>0; k--) {
+            int i = generator.nextInt(k);
+            int itemid = this.arr[i];
+            this.arr[i] = this.arr[k];
+            this.arr[k] = itemid;
+        }
+
     }
 
     public int[] toArray() {
