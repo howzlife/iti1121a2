fb70f0db9b3a6b967a0188367f8e0ae990799c33
Uiri <uiri00@gmail.com>
2014-02-06 21:57:33 -0500

    Make clicking the buttons work
    When a button is clicked and it is beside the null square, it is
    supposed to trade places with the null square. This happens in the
    Board's cell array and is then updated on screen. If the puzzle is
    solved, a dialog saying "You Won!" pops up.

--- Board.java
+++ Board.java
@@ -1,4 +1,5 @@
 import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 import java.awt.event.*;
 import java.awt.GridLayout;
 
@@ -13,8 +14,50 @@ class Board extends JPanel implements ActionListener {
     }
     
     public void actionPerformed(ActionEvent e) {
-        if (!(e.source instanceof Cell))
+        int i, j, pos, swap = -1;
+        Cell c;
+        boolean youwon;
+
+        if (!(e.getSource() instanceof Cell))
             return;
+        youwon = true;
+        c = (Cell)e.getSource();
+        pos = -1;
+        if (c.getId() == 0)
+            return;
+        for (i=0;i<this.cells.length;i++) {
+            if (this.cells[i].getId() == c.getId()) {
+                pos = i;
+                break;
+            }
+        }
+        if (pos > 2 && this.cells[pos-3].getId() == 0) {
+            swap = pos-3;
+        } else if(pos > 0 && this.cells[pos-1].getId() == 0) {
+            swap = pos-1;
+        } else if (pos < 8 && this.cells[pos+1].getId() == 0) {
+            swap = pos+1;
+        } else if (pos < 6 && this.cells[pos+3].getId() == 0) {
+            swap = pos+3;
+        }
+        if (swap<0)
+            return;
+        this.cells[pos] = this.cells[swap];
+        this.cells[swap] = c;
+        this.removeAll();
+        for (i=0;i<3;i++) {
+            for (j=0;j<3;j++) {
+                this.add(cells[i*3 + j]);
+                if (this.cells[i*3 + j].getRow() != j ||
+                    this.cells[i*3 + j].getColumn() != i)
+                    youwon = false;
+            }
+        }
+        this.revalidate();
+        this.repaint();
+        if (youwon) {
+            JOptionPane.showMessageDialog(this, "You won!");
+        }
     }
 
     public void init() {
