e524c3fad37a5772f500db0e091d7389291eb851
Uiri <uiri00@gmail.com>
2014-02-25 17:35:15 -0500

    Add StudentInfo

---
+++ StudentInfo.java
@@ -0,0 +1,27 @@
+/**
+ * All the main methods of this assignment must call the method
+ * StudentInfo.display().
+ * 
+ * @author Marcel Turcotte and Nathalie Japkowicz
+ */
+
+public class StudentInfo {
+
+    /**
+     * Displays the student information: student name, id, section, etc. You
+     * must fill this information inside the box.
+     */
+
+    public static void display() {
+
+        System.out.println("************************************************************");
+        System.out.println("*                 Author: William Pearson, Nicolas Dubus   *");
+        System.out.println("*                 Student number: 7204884 , 3811637        *");
+        System.out.println("*                     Course: ITI1121                      *");
+        System.out.println("*                      Assignment: 2                       *");
+        System.out.println("************************************************************");
+        System.out.println();
+
+    }
+
+}
